f5c5299c5384d7527d7ef52152e3afa23b55140f
Vicuko <victorc.fernandez@gmail.com>
2017-08-10 21:54:47 +0200

    Create and define News class

---
+++ app/src/main/java/com/example/android/newsapp/News.java
@@ -0,0 +1,39 @@
+package com.example.android.newsapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Vicuko on 10/8/17.
+ */
+
+public class News {
+    private String mWebTitle;
+    private ArrayList<String> mAuthors;
+    private String mSectionName;
+    private String mPublishedDate;
+
+    public News(String title, ArrayList<String> authors, String description,
+                String publishedDate){
+
+        mWebTitle = title;
+        mAuthors = authors;
+        mSectionName = description;
+        mPublishedDate = publishedDate;
+    }
+
+    public String getTitle(){
+        return mWebTitle;
+    }
+
+    public ArrayList<String> getAuthors(){
+        return mAuthors;
+    }
+
+    public String getSectionName(){
+        return mSectionName;
+    }
+
+    public String getPublishedDate(){
+        return mPublishedDate;
+    }
+}
